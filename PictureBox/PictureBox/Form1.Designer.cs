e33564cbfa50f80c29303741ac85256fbed0e5fd
Beab2217 <57806757+Beab2217@users.noreply.github.com>
2020-05-22 20:38:36 +0300

    Add files via upload

---
+++ PictureBox/PictureBox/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(503, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create the graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(284, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "From file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(504, 46);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(283, 69);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(570, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(504, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(503, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(503, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(284, 26);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(504, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PictureBox/PictureBox/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private double step;
+        private List<int> num;
+        private List<float> xx = new List<float>();
+        private List<float> yy = new List<float>();
+        private int xyStep;
+        private string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Visible = true;
+            textBox2.Text = "0";
+            label1.Text = "Step = ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            
+            textBox1.Visible = true;
+
+            graphics = pictureBox1.CreateGraphics();
+
+            int w = pictureBox1.Width / 2;
+            int h = pictureBox1.Height / 2;
+
+            PointF[] point = new PointF[w];
+
+            graphics.TranslateTransform(w, h);
+
+            DrawXAxis(new Point(-w, 0), new Point(w, 0), graphics);
+            DrawYAxis(new Point(0, h), new Point(0, -h), graphics);
+
+            graphics.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+
+            for (double i = -step*5; i < step*5; i+=step/5)
+            {
+                xx.Add((float)i);
+            }
+            for (int i = 0; i < xx.Count; i++)
+            {
+                double res = 0;
+                for (int j = 0; j < num.Count; j++)
+                {
+                    res += num[j] * Math.Pow(xx[i], num.Count - j - 1);
+                }
+                yy.Add(-(float)res);
+            }
+            for (int i = 0; i < xx.Count-1; i++)
+            {
+                float t = (float) (pictureBox1.Height / (10 * step));
+                graphics.DrawLine(Pens.Black, xx[i]*t, yy[i]*t,xx[i+1]*t, yy[i+1]*t);
+            }
+        }
+
+        private void DrawXAxis(Point start, Point end, Graphics g)
+        {
+            double xStep = step;
+            for (int i = xyStep; i < end.X; i += xyStep)
+            {
+                g.DrawLine(Pens.Black, i, -5, i, 5);
+                DrawText(new Point(i, 5), (xStep).ToString(), g);
+                xStep += step;
+            }
+
+            xStep = -step;
+            for (int i = -xyStep; i > start.X; i -= xyStep)
+            {
+                g.DrawLine(Pens.Black, i, -5, i, 5);
+                DrawText(new Point(i, 5), (xStep).ToString(), g);
+                xStep -= step;
+            }
+
+            g.DrawLine(Pens.Black, start, end);
+        }
+
+        private void DrawYAxis(Point start, Point end, Graphics g)
+        {
+            double yStep = step;
+            for (int i = xyStep; i < start.Y; i += xyStep)
+            {
+                g.DrawLine(Pens.Black, -5, i, 5, i);
+                DrawText(new Point(5, i), (-yStep).ToString(), g, true);
+                yStep += step;
+            }
+
+            yStep = -step;
+            for (int i = -xyStep; i > end.Y; i -= xyStep)
+            {
+                g.DrawLine(Pens.Black, -5, i, 5, i);
+                DrawText(new Point(5, i), (-yStep).ToString(), g, true);
+                yStep -= step;
+            }
+
+            g.DrawLine(Pens.Black, start, end);
+        }
+
+        private void DrawText(Point point, string text, Graphics g, bool isYAxis = false)
+        {
+            var f = new Font(Font.FontFamily, 6);
+            var size = g.MeasureString(text, f);
+            var pt = isYAxis
+                ? new PointF(point.X + 1, point.Y - size.Height / 2)
+                : new PointF(point.X - size.Width / 2, point.Y + 1);
+            var rect = new RectangleF(pt, size);
+            g.DrawString(text, f, Brushes.Black, rect);
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+            step = trackBar1.Value;
+            xyStep = pictureBox1.Height / 8;
+        }
+
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Int32.Parse(textBox2.Text) > 50)
+                {
+                    trackBar1.Value = 50;
+                    textBox2.Text = "50";
+                    step = 50;
+                }
+
+                else if (Int32.Parse(textBox2.Text) < 0)
+                {
+                    trackBar1.Value = 0;
+                    textBox2.Text = "0";
+                    step = 0;
+                }
+
+                else
+                {
+                    trackBar1.Value = Int32.Parse(textBox2.Text);
+                    step = Int32.Parse(textBox2.Text);
+                }
+            }
+            catch (Exception exception)
+            {
+                textBox2.Text = "0";
+                step = 0;
+                return;
+            }
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            num = new List<int>();
+            String str = textBox1.Text, res = "";
+            char[] chars = str.ToCharArray();
+            bool sign = false;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] != '!')
+                {
+                    if (chars[i] == '-')
+                    {
+                        sign = true;
+                    }
+                    else
+                    {
+                        res += chars[i];
+                    }
+                }
+
+                if (chars[i] == '!')
+                {
+                    if (sign == true)
+                    {
+                        num.Add(-Int32.Parse(res));
+                    }
+                    else
+                    {
+                        num.Add(Int32.Parse(res));
+                    }
+                    sign = false;
+                    res = null;
+                }
+            }
+
+            String ss = null;
+            for (int i = 0; i < num.Count; i++)
+            {
+                ss += num[i] + "x^" + (num.Count - i - 1);
+                ss += " + ";
+            }
+            label2.Text = ss;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+
+            textBox1.Visible = true;
+
+            graphics = pictureBox1.CreateGraphics();
+
+            int w = pictureBox1.Width / 2;
+            int h = pictureBox1.Height / 2;
+
+            PointF[] point = new PointF[w];
+
+            graphics.TranslateTransform(w, h);
+
+            DrawXAxis(new Point(-w, 0), new Point(w, 0), graphics);
+            DrawYAxis(new Point(0, h), new Point(0, -h), graphics);
+
+            graphics.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+            
+            fileName = textBox3.Text;
+            StreamReader streamReader = new StreamReader(fileName, System.Text.Encoding.Default);
+            string line = null, x = null, y = null;
+            SortedDictionary<float, float> xyFile = new SortedDictionary<float, float>();
+
+            while ((line = streamReader.ReadLine())!=null)
+            {
+                Console.WriteLine(line);
+                x = line.Substring(0, line.IndexOf(" ")).Trim();
+                y = line.Substring(line.IndexOf(" ")).Trim();
+                xyFile.Add(float.Parse(x), float.Parse(y));
+            }
+
+            streamReader.Close();
+
+            String xFile = "x: ", yFile = "y: ";
+            foreach (var keys in xyFile)
+            {
+                xFile += keys.Key + " ";
+                yFile += keys.Value + " ";
+            }
+            label3.Text = xFile + '\n' + yFile;
+
+            float xX = 0;
+            float yY = 0;
+            foreach (var keys in xyFile)
+            {
+                xx.Add(keys.Key);
+                yy.Add(keys.Value);
+            }
+
+            for (int i = 0; i < xx.Count - 1; i++)
+            {
+                float t = (float)(pictureBox1.Height / (step * 10));
+                graphics.DrawLine(Pens.Black, xx[i] * t, yy[i] * t, xx[i + 1] * t, yy[i + 1] * t);
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
